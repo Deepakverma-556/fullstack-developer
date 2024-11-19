fdbbf011df8b4505cdfd426bb4671c967e88afe8
unknown <deepakradialcode@gmail.com>
2024-11-19 14:31:52 +0530

    add css classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: '#70ff00',
+        dark: '#161616',
+        light: '#e0e0e0',
+        'dark-gray': '#828282',
+        'light-gray': '#f2f2f2',
+        'medium-gray':'#4f4f4f',
       },
+      fontSize:{
+        xsm: '26px',
+        medium: '33px',
+        xxl:'50px',
+      },
+      lineHeight: {
+        xl: '33px',
+        xxl:'91px',
+      }
     },
   },
   plugins: [],
