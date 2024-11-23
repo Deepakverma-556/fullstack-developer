30e7b94880637ddfd1a3a6571f91ff24d9b94dd1
unknown <deepakradialcode@gmail.com>
2024-11-23 09:58:46 +0530

    scroll smooth on click

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-"use client"
-import { useEffect } from "react"
+"use client";
+import { useEffect } from "react";
 import Main from "@/components/Main";
 import Lenis from "lenis";
 import Bottom from "@/components/Bottom";
@@ -11,23 +11,47 @@ import Projects from "@/components/Projects";
 export default function Home() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.5, easing:
-        (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
-      direction: 'vertical'
+      direction: "vertical",
     });
+
+    const handleAnchorClick = (event) => {
+      const targetId = event.target.getAttribute("href");
+      if (targetId && targetId.startsWith("#")) {
+        event.preventDefault();
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          lenis.scrollTo(targetElement);
+        }
+      }
+    };
+
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+    anchorLinks.forEach((link) =>
+      link.addEventListener("click", handleAnchorClick)
+    );
+
     const raf = (time) => {
       lenis.raf(time);
       requestAnimationFrame(raf);
     };
     requestAnimationFrame(raf);
-    return () => { lenis.destroy(); };
+
+    return () => {
+      lenis.destroy();
+      anchorLinks.forEach((link) =>
+        link.removeEventListener("click", handleAnchorClick)
+      );
+    };
   }, []);
+
   return (
     <>
       <Main />
       <Skills />
-      <Projects/>
+      <Projects />
       <Faq />
       <Footer />
       <Bottom />
